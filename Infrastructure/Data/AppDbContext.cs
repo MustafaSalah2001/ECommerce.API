04c8689611ec7a6a7792ffa957d4b6572dc50a54
Mustafa_Salah <101256406+mustafasalah21@users.noreply.github.com>
2026-05-20 09:27:09 +0300

    Initial e-commerce project

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+}
