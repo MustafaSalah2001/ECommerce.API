b27b131bdcfa4aba972a51d5cdad0a6df05264a6
Mustafa_Salah <101256406+mustafasalah21@users.noreply.github.com>
2026-05-23 16:29:16 +0300

    all crud opration to catergories

--- ECommerce.API/Controllers/CategoriesController.cs
+++ ECommerce.API/Controllers/CategoriesController.cs
@@ -46,5 +46,38 @@ namespace ECommerce.API.Controllers
 
             return CreatedAtAction(nameof(GetCategory), new { id = category.Id }, category);
         }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCategory(int id, UpdateCategoryDto dto)
+        {
+            var existingCategory = await _context.Categories.FindAsync(id);
+
+            if (existingCategory == null)
+            {
+                return NotFound();
+            }
+
+            existingCategory.Name = dto.Name;
+            existingCategory.Description = dto.Description;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
